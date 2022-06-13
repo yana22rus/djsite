70b0b61f717b325f6f1aed514e740a111517b9ee
qwe <test@mail.ru>
2022-06-13 13:34:35 +0300

    startproject

---
+++ myapp/tag_documents/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TagDocumentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tag_documents'
---
+++ myapp/tag_news/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TagNewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tag_news'
